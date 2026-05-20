26d06ee154ec6c159b138c48b2e6f19c71918876
Siddiq Ahmed khan <siddiqahmedkhann@gmail.com>
2026-05-19 23:13:47 -0400

    Revise About section with updated personal info
    Updated the About component to reflect new experience and education details.

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -22,11 +22,12 @@ function About() {
       <div className="about-grid">
         <div className="about-grid-info">
           <p className="about-grid-info-text">
-          I'm a 2021 computer science graduate who discovered my passion for coding during the pandemic.
-          With online learning on the rise, I took the opportunity to self-learn and enrolled in various programming courses.
-          Through self-directed study, I've developed my skills and earned my knowledge. After completing my education, 
+          Web Developer with  2+ Years of Experiance,
+          Masters in Information Science, and Computer science graduate who discovered his passion for coding and Building Web App's.
+          With AI on the rise, I took the opportunity to self-learn and enrolled in various programming and AI courses.
+          Through self-directed study, I've developed my skills and earned my knowledge.
           I've gained valuable experience in the IT sector. Now, 
-          I'm focused on building a career as a Web3 developer and working towards making my dream a reality.
+          I'm focused on building a career as a AI Web developer and working towards making my dream a reality.
           </p>
 
           <p className="about-grid-info-text">
@@ -35,8 +36,8 @@ function About() {
           </p>
 
           <p className="about-grid-info-text">
-            I completed my under-graduate education in 2021 and have gained valuable experience
-            in IT Sector for almost 2 years. Now I aim to complete my graduate studies and establish myself as a full Stack developer.
+            Recently completed my Masters in 2025 and under-graduate education in 2021 and have gained valuable experience
+            in IT Sector for almost 2 years. Now I aim to establish myself as a full Stack developer.
             and I am on my way to make my dream come true!
           </p>
           <ul className="about-grid-info-list">
